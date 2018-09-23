7a1e5101dcd657828cedf57db9738dda0db9c3b3
JK Jung <jkjung13@gmail.com>
2018-09-23 12:39:26 +0800

    Add sss_inception_v2_egohands config and the training script

--- train.sh
+++ train.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-export PYTHONPATH=`pwd`/models/research:`pwd`/models/research/slim
+usage()
+{
+    echo "Usage: (one of the following)"
+    echo "    $ ./train.sh ssd_mobilenet_v1_egohands"
+    echo "    $ ./train.sh ssd_inception_v2_egohands"
+    exit
+}
 
+if [ $# -ne 1 ]; then
+    usage
+fi
+
+case $1 in
+    ssd_mobilenet_v1_egohands )
+        PIPELINE_CONFIG_PATH=configs/ssd_mobilenet_v1_egohands.config
+        MODEL_DIR=ssd_mobilenet_v1_egohands
+        NUM_TRAIN_STEPS=20000
+        ;;
+    ssd_inception_v2_egohands )
+        PIPELINE_CONFIG_PATH=configs/ssd_inception_v2_egohands.config
+        MODEL_DIR=ssd_inception_v2_egohands
+        NUM_TRAIN_STEPS=20000
+        ;;
+    * )
+        usage
+esac
+
+PYTHONPATH=`pwd`/models/research:`pwd`/models/research/slim \
 python3 ./models/research/object_detection/model_main.py \
-        --pipeline_config_path=configs/ssd_mobilenet_v1_egohands.config \
-        --model_dir=ssd_mobilenet_v1_egohands \
-        --num_train_steps=20000 \
+        --pipeline_config_path=$PIPELINE_CONFIG_PATH \
+        --model_dir=$MODEL_DIR \
+        --num_train_steps=$NUM_TRAIN_STEPS \
         --sample_1_of_n_eval_samples=1 \
         --alsologtostderr
