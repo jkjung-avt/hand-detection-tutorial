ce26361263088ca3dff3a8725b1e7da396491d37
JK Jung <jkjung13@gmail.com>
2018-09-25 12:06:12 +0800

    Fix PYTHONPATH in create_tfrecords.sh

--- create_tfrecords.sh
+++ create_tfrecords.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PYTHONPATH=$MODELS_DIR/research:$MODELS_DIR/research/slim \
+PYTHONPATH=`pwd`/models/research:`pwd`/models/research/slim \
     python3 create_kitti_tf_record.py \
             --data_dir=egohands_kitti_formatted \
             --output_path=data/egohands \
