1075949cd4a17212fd2275956e37aeb0880128d6
JK Jung <jkjung13@gmail.com>
2018-10-04 13:31:29 +0800

    Refactor train.sh and eval.sh

--- train.sh
+++ train.sh
@@ -24,59 +24,34 @@ if [ $# -ne 1 ]; then
 fi
 
 case $1 in
-    ssd_mobilenet_v1_egohands )
-        PIPELINE_CONFIG_PATH=configs/ssd_mobilenet_v1_egohands.config
-        MODEL_DIR=ssd_mobilenet_v1_egohands
-        NUM_TRAIN_STEPS=20000
-        ;;
-    ssd_mobilenet_v2_egohands )
-        PIPELINE_CONFIG_PATH=configs/ssd_mobilenet_v2_egohands.config
-        MODEL_DIR=ssd_mobilenet_v2_egohands
-        NUM_TRAIN_STEPS=20000
-        ;;
-    ssdlite_mobilenet_v2_egohands )
-        PIPELINE_CONFIG_PATH=configs/ssdlite_mobilenet_v2_egohands.config
-        MODEL_DIR=ssdlite_mobilenet_v2_egohands
-        NUM_TRAIN_STEPS=20000
-        ;;
+    ssd_mobilenet_v1_egohands | \
+    ssd_mobilenet_v2_egohands | \
+    ssdlite_mobilenet_v2_egohands | \
     ssd_inception_v2_egohands )
-        PIPELINE_CONFIG_PATH=configs/ssd_inception_v2_egohands.config
-        MODEL_DIR=ssd_inception_v2_egohands
+        MODEL_DIR=$1
         NUM_TRAIN_STEPS=20000
         ;;
-    rfcn_resnet101_egohands )
-        PIPELINE_CONFIG_PATH=configs/rfcn_resnet101_egohands.config
-        MODEL_DIR=rfcn_resnet101_egohands
-        NUM_TRAIN_STEPS=50000
-        ;;
-    faster_rcnn_resnet50_egohands )
-        PIPELINE_CONFIG_PATH=configs/faster_rcnn_resnet50_egohands.config
-        MODEL_DIR=faster_rcnn_resnet50_egohands
-        NUM_TRAIN_STEPS=50000
-        ;;
-    faster_rcnn_resnet101_egohands )
-        PIPELINE_CONFIG_PATH=configs/faster_rcnn_resnet101_egohands.config
-        MODEL_DIR=faster_rcnn_resnet101_egohands
-        NUM_TRAIN_STEPS=50000
-        ;;
-    faster_rcnn_inception_v2_egohands )
-        PIPELINE_CONFIG_PATH=configs/faster_rcnn_inception_v2_egohands.config
-        MODEL_DIR=faster_rcnn_inception_v2_egohands
-        NUM_TRAIN_STEPS=50000
-        ;;
+    rfcn_resnet101_egohands | \
+    faster_rcnn_resnet50_egohands | \
+    faster_rcnn_resnet101_egohands | \
+    faster_rcnn_inception_v2_egohands | \
     faster_rcnn_inception_resnet_v2_atrous_egohands )
-        PIPELINE_CONFIG_PATH=configs/faster_rcnn_inception_resnet_v2_atrous_egohands.config
-        MODEL_DIR=faster_rcnn_inception_resnet_v2_atrous_egohands
+        MODEL_DIR=$1
         NUM_TRAIN_STEPS=50000
         ;;
     * )
         usage
 esac
 
+PIPELINE_CONFIG_PATH=configs/${MODEL_DIR}.config
+
+# clear old training logs
+rm -rf ${MODEL_DIR}
+
 PYTHONPATH=`pwd`/models/research:`pwd`/models/research/slim \
     python3 ./models/research/object_detection/model_main.py \
-            --pipeline_config_path=$PIPELINE_CONFIG_PATH \
-            --model_dir=$MODEL_DIR \
-            --num_train_steps=$NUM_TRAIN_STEPS \
+            --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
+            --model_dir=${MODEL_DIR} \
+            --num_train_steps=${NUM_TRAIN_STEPS} \
             --sample_1_of_n_eval_samples=1 \
             --alsologtostderr
